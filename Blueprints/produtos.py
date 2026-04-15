ae2c007335b6eaea1598bb42a3f518df47f93282
TallysASK <tallyskeneuber@gmail.com>
2026-04-14 21:03:54 -0400

    melhorando organização das rotas de produtos

---
+++ Blueprints/produtos.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, request, url_for, session, redirect, Blueprint, abort
+from flask_login import login_required, login_user, logout_user, current_user
+from db import db
+from models import Loja, Produtos
+
+prod_bp = Blueprint("prod", __name__)
+
+# Página de produtos
+   
+@prod_bp.route("/produtos/<loja>", methods=["GET", "POST"])
+def produtos(loja):    
+    loja_obj = Loja.query.filter_by(username=loja).first()
+    
+    if not loja_obj:
+        abort(404)
+        
+    produtos = loja_obj.produtos
+    
+    return render_template("produtos/produtos.html", loja=loja_obj, produtos=produtos)
+    
+# Adicionar produtos
+
+@prod_bp.route("/produtos/add",  methods=["POST"])
+@login_required
+def addprodutos():
+    addnome = request.form.get("nomeProdutoAdd", "").strip()
+    addpreco = request.form.get("precoProdutoAdd")        
+    addquant = request.form.get("quantProdutoAdd")                
+    addmostrar = request.form.get("mostrarProdutoAdd") == "Sim"
+ 
+    if not addnome:
+        return "Nome do produto obrigatório", 400        
+    addpreco = float(addpreco) if addpreco else 0
+    addquant = int(addquant) if addquant else 0
+        
+    produto_novo = Produtos(
+    nome=addnome, 
+    preco=addpreco, 
+    quantidade=addquant, 
+    mostrar=addmostrar, loja_id=current_user.id
+        )
+        
+    db.session.add(produto_novo)
+    db.session.commit()
+        
+    return redirect(url_for("prod.produtos", loja=current_user.username))
+    
+# Editar produtos
+
+@prod_bp.route("/produtos/edit", methods=["GET", "POST"])
+@login_required
+def editprodutos():
+    produtos = Produtos.query.filter_by(loja_id=current_user.id).all()
+
+    if request.method == "POST":
+
+        for produto in produtos:
+          
+          #Excluir produto
+          
+            deletar_produto = request.form.get(f"delproduto_{produto.id}")
+            
+            if deletar_produto == "excluir produto":
+                db.session.delete(produto)
+                continue
+                
+          #Editar
+          
+            nome = request.form.get(f"nomeProduto_{produto.id}", "").strip()
+            preco = request.form.get(f"precoProduto_{produto.id}")
+            quant = request.form.get(f"quantProduto_{produto.id}")
+            mostrar = request.form.get(f"mostrarProduto_{produto.id}")
+    
+            if nome:
+                produto.nome = nome
+    
+            if preco:
+                produto.preco = float(preco)
+    
+            if quant:
+                produto.quantidade = int(quant)
+                    
+            if mostrar is not None:
+                produto.mostrar = (mostrar == "Sim")
+  
+        db.session.commit()
+        return redirect(url_for("prod.produtos", loja=current_user.username))
+        
+    return render_template("produtos/edit_produtos.html", produtos=produtos)
